63d49c81d3d0ff17ecabf5f81d9edd08b04ea88b
anbi <biswaszin@gmail.com>
2026-06-18 19:49:57 +0530

    rando commit

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-let i = 0;
+let x = 0;
 
-while (i < 3) {
-  alert(i);
-  i++;
-}
-
-for (let j = 2; j <= 10; j++) {
-  if (j % 2 == 0) {
-    alert(j);
-  }
-}
-
-let k = 0;
-
-while (k < 3) {
-  alert(`number ${k}!`);
-
-  k++;
+while (x < 3) {
+  console.log(x);
+  x++;
 }

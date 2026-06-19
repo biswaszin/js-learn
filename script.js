24953caefed3fad918021dd5e609405d8c6c7d53
anbi <biswaszin@gmail.com>
2026-06-19 17:57:09 +0530

    Add default param

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-let x = 0;
+let askEmail = prompt("What's your email address?");
+let name = prompt("Could you mention your name too?");
 
-while (x < 3) {
-  console.log(x);
-  x++;
+let userEmail = askEmail;
+let userName = name;
+
+function getGreeting(email, name = "there") {
+  console.log(`Hello ${name}, welcome! You've registered your email: ${email}`);
 }
 
-function add(a, b) {
-  return a + b;
-}
-
-let sum = add(5, 8);
-console.log(sum);
-
-let personName = prompt("What's your name? ");
-
-function greet(name) {
-  alert(`Hey there! Welcome ${name}`);
-}
-
-greet(personName);
-
-let fruits = ["apple", "banana", "orange"];
-console.log(fruits[0]);
-
-// PUSH() :adds an elem to the end of the array
-// POP() :removes the last elem
-// SHIFT() :removes the first elem of the array
-// UNSHIFT() :adds an elem in the beginning of the array
-// SLICE() :extracts a portion of the array
-// forEach() :executes a function once for each element in the array
+getGreeting(userEmail, userName);

8dd8a667319c992d3079f5b1d3ad1af5ae657abe
anbi <biswaszin@gmail.com>
2026-06-24 20:57:46 +0530

    cleanup

--- js-files/extras.js
+++ js-files/extras.js
@@ -11,3 +11,17 @@ map.set("bertholdt", "colossal titan");
 console.log(map);
 
 // PROMISES / ASYNC / AWAIT
+
+function placeOrder(pizza) {
+  return new Promise((resolve, reject) => {
+    console.log(`Placing Order for ${pizza}`);
+
+    setTimeout(() => {
+      if (pizza === "Margherita") {
+        resolve("Order Confirmed! ETA 30min"); // Success
+      } else {
+        reject("Sorry, we only have Margherita"); // Failure
+      }
+    }, 1500);
+  });
+}

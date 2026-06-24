1e095672a7c76476b1c07836a56e9489a83784ec
anbi <biswaszin@gmail.com>
2026-06-24 15:59:26 +0530

    cleanup

---
+++ js-files/extras.js
@@ -0,0 +1,13 @@
+// maps
+// like dictionaries, key value pairs
+// except can only hold unique keys
+
+const map = new Map();
+map.set("bertholdt", "shifter");
+map.set("reiner", "warrior");
+map.set("annie", "shifter");
+map.set("bertholdt", "colossal titan");
+
+console.log(map);
+
+// PROMISES / ASYNC / AWAIT
